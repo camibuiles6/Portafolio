d5458fbbfe829a1a29a01385f3cecb68cbcbd0a8
suradual <suradual@gmail.com>
2024-03-06 17:54:29 -0500

    ajustes

---
+++ maquinaescribir.js
@@ -0,0 +1,14 @@
+ // typing text animation script
+ var typed = new Typed(".typing", {
+    strings: ["Engineer", "Developer", "Student", "Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing-2", {
+    strings: ["Engineer", "Developer", "Student", "Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
